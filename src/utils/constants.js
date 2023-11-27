c7be71d237f80a96db926bbc360108c4a739bd8b
Toni Rondán <131011220+Kaysera14@users.noreply.github.com>
2023-11-27 16:56:00 +0100

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const CURRENT_USER_STORAGE_ID = "userToken";
 
-export const API_HOST = "http://retrotech-shop.alwaysdata.net";
+export const API_HOST = "https://retrotech-shop.alwaysdata.net";
 
 //export const API_HOST = "http://localhost:3000";
